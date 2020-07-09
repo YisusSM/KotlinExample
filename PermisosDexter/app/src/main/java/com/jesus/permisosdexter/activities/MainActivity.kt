13f2ca9775bb8f62da9456820b5aac0a57e8d325
TheBlockCode <xxchuchoxd@outlook.com>
2020-07-09 02:47:35 -0500

    1.0.5 Permissions Composite

--- PermisosDexter/app/src/main/java/com/jesus/permisosdexter/activities/MainActivity.kt
+++ PermisosDexter/app/src/main/java/com/jesus/permisosdexter/activities/MainActivity.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import com.jesus.permisosdexter.R
 import com.jesus.permisosdexter.enums.PermissionStatusEnum
 import com.karumi.dexter.Dexter
@@ -14,7 +15,11 @@ import com.karumi.dexter.listener.PermissionDeniedResponse
 import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.multi.SnackbarOnAnyDeniedMultiplePermissionsListener
+import com.karumi.dexter.listener.single.CompositePermissionListener
+import com.karumi.dexter.listener.single.DialogOnDeniedPermissionListener
 import com.karumi.dexter.listener.single.PermissionListener
+import com.karumi.dexter.listener.single.SnackbarOnDeniedPermissionListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -26,15 +31,16 @@ class MainActivity : Activity() {
         setButtonClicks()
 
 
-
     }
 
     private fun setButtonClicks() {
         buttonCamera.setOnClickListener {
-            checkPermissions(
-                Manifest.permission.CAMERA,
-                textViewCamera
-            )
+            /* checkPermissions(
+                 Manifest.permission.CAMERA,
+                 textViewCamera
+             )*/
+           // setCameraPermissionHandlerWithDialog()
+            setCameraPermissionHandlerWithSnackbar()
         }
         buttonContact.setOnClickListener {
             checkPermissions(
@@ -186,6 +192,112 @@ class MainActivity : Activity() {
     private fun setPermissionStatus(textView: TextView, status: Int, statusColor: Int) {
         textView.setText(status)
         textView.setTextColor(ContextCompat.getColor(applicationContext, statusColor))
+    }
+
+    private fun setCameraPermissionHandlerWithDialog() {
+        val dialogoPermissionListener = DialogOnDeniedPermissionListener.Builder.withContext(this)
+            .withTitle("Camera Permission")
+            .withMessage("Camera Permission is needed to take pictures")
+            .withButtonText(android.R.string.ok)
+            .withIcon(R.mipmap.ic_launcher_round)
+            .build()
+
+        val permission = object : PermissionListener {
+            override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                setPermissionStatus(
+                    textViewCamera,
+                    R.string.permission_status_granted,
+                    R.color.colorPermissionGranted
+                )
+            }
+
+            override fun onPermissionDenied(p0: PermissionDeniedResponse) {
+                if (!p0.isPermanentlyDenied) {
+                    setPermissionStatus(
+                        textViewCamera,
+                        R.string.permission_status_denied,
+                        R.color.colorPermissionDenied
+                    )
+                } else {
+                    setPermissionStatus(
+                        textViewCamera,
+                        R.string.permission_status_denied_permanently,
+                        R.color.colorPermissionStatusPermanentlyDenied
+                    )
+                }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                p0: PermissionRequest?,
+                p1: PermissionToken?
+            ) {
+                p1?.continuePermissionRequest()
+            }
+
+
+        }
+        val composite = CompositePermissionListener(permission, dialogoPermissionListener)
+        Dexter.withActivity(this)
+            .withPermission(Manifest.permission.CAMERA)
+            .withListener(composite)
+            .check()
+
+    }
+
+    private fun setCameraPermissionHandlerWithSnackbar() {
+        val snackbarPermissionListener = SnackbarOnDeniedPermissionListener.Builder.with(
+            root,
+            "Camera is needed to take pictures!"
+        ).withOpenSettingsButton("settings")
+            .withCallback(object: Snackbar.Callback(){
+                override fun onShown(sb: Snackbar?) {
+
+                }
+
+                override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
+
+                }
+            }).build()
+
+        val permission = object : PermissionListener {
+            override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                setPermissionStatus(
+                    textViewCamera,
+                    R.string.permission_status_granted,
+                    R.color.colorPermissionGranted
+                )
+            }
+
+            override fun onPermissionDenied(p0: PermissionDeniedResponse) {
+                if (!p0.isPermanentlyDenied) {
+                    setPermissionStatus(
+                        textViewCamera,
+                        R.string.permission_status_denied,
+                        R.color.colorPermissionDenied
+                    )
+                } else {
+                    setPermissionStatus(
+                        textViewCamera,
+                        R.string.permission_status_denied_permanently,
+                        R.color.colorPermissionStatusPermanentlyDenied
+                    )
+                }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                p0: PermissionRequest?,
+                p1: PermissionToken?
+            ) {
+                p1?.continuePermissionRequest()
+            }
+
+
+        }
+        val composite = CompositePermissionListener(permission,snackbarPermissionListener)
+        Dexter.withActivity(this)
+            .withPermission(Manifest.permission.CAMERA)
+            .withListener(composite)
+            .check()
     }
 
 
