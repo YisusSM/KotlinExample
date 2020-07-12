5f0d37bc3a4dda865f823bcbfae4fb7456359adc
TheBlockCode <xxchuchoxd@outlook.com>
2020-07-11 21:25:35 -0500

    Refactor

--- Drawer_Recycler_Card/app/src/main/java/com/jesus/drawer_recycler_card/activities/MainActivity.kt
+++ Drawer_Recycler_Card/app/src/main/java/com/jesus/drawer_recycler_card/activities/MainActivity.kt
@@ -2,8 +2,12 @@ package com.jesus.drawer_recycler_card.activities
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.WindowId
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 import com.jesus.drawer_recycler_card.R
@@ -11,6 +15,8 @@ import com.jesus.drawer_recycler_card.fragments.arrivalsFragment
 import com.jesus.drawer_recycler_card.fragments.departuresFragment
 import com.jesus.drawer_recycler_card.fragments.homeFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.nav_header.view.*
+import org.w3c.dom.Text
 
 class MainActivity : ToolBarActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +25,24 @@ class MainActivity : ToolBarActivity(), NavigationView.OnNavigationItemSelectedL
 
         toolbarToLoad(tooolbar as Toolbar)
         setNavDrawer()
-        fragmentTransaction(homeFragment())
-        navView.menu.getItem(0).isChecked = true //Cuando inicias la app deja marcado la primera opcion del navigator
+        setUserInformation()
 
+
+        if (savedInstanceState == null) {
+            fragmentTransaction(homeFragment())
+            navView.menu.getItem(0).isChecked =
+                true //Cuando inicias la app deja marcado la primera opcion del navigator
+        } 
+    }
+
+    private fun closeNavigate() = drawerLayout.closeDrawer(GravityCompat.START)
+
+    private fun setUserInformation() {
+        val name = navView.getHeaderView(0).textViewName
+        val email = navView.getHeaderView(0).textViewEmail
+
+        name?.let { name.text = getString(R.string.user_name) }
+        email?.let { email.text = getString(R.string.user_email) }
 
     }
 
@@ -40,17 +61,53 @@ class MainActivity : ToolBarActivity(), NavigationView.OnNavigationItemSelectedL
         navView.setNavigationItemSelectedListener(this)
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.nav_home -> fragmentTransaction(homeFragment())
-            R.id.nav_departures -> fragmentTransaction(departuresFragment())
-            R.id.nav_arrivals -> fragmentTransaction(arrivalsFragment())
+    private fun loadFragmentById(id: Int) {
+        when (id) {
+            R.id.nav_home -> {
+                fragmentTransaction(homeFragment())
+                closeNavigate()
+            }
+            R.id.nav_departures -> {
+                fragmentTransaction(departuresFragment())
+                closeNavigate()
+            }
+            R.id.nav_arrivals -> {
+                fragmentTransaction(arrivalsFragment())
+                closeNavigate()
+            }
         }
+    }
+
+    private fun showMessageNavItemSelectedById(id: Int) {
+        when (id) {
+            R.id.nav_profile -> makeToast("Profile")
+            R.id.nav_settings -> makeToast("Settings")
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        showMessageNavItemSelectedById(item.itemId)
+        loadFragmentById(item.itemId)
+
         return true
     }
-    private fun fragmentTransaction(fragment:Fragment){
+
+    private fun fragmentTransaction(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
-            .replace(R.id.container,fragment)
+            .replace(R.id.container, fragment)
             .commit()
     }
+
+    private fun makeToast(msj: String) {
+        Toast.makeText(this, msj, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
 }
