e2664d2c62660efac725040916498c755eec1ccf
TheBlockCode <xxchuchoxd@outlook.com>
2020-07-11 19:33:47 -0500

    Navigation Drawer Add

--- Drawer_Recycler_Card/app/src/main/java/com/jesus/drawer_recycler_card/MainActivity.kt
+++ Drawer_Recycler_Card/app/src/main/java/com/jesus/drawer_recycler_card/MainActivity.kt
@@ -2,10 +2,15 @@ package com.jesus.drawer_recycler_card
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ToolBarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbarToLoad(tooolbar as Toolbar)
+
     }
 }
